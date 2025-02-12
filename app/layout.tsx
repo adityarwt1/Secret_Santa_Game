5156d6095f99340d07ae021d20a6462681bcbdc7
adityarwt1 <adityarawatnew2487@gmail.com>
2025-02-12 14:29:40 +0530

    fullly working csv to secret santa file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Santa Secret App',
+  description: 'Santa Secrete app CSV File made Aditya Rawat Fullstack Engineer',
+  keywords:"secret santa game, secrete santa game, code challenge"
 }
 
 export default function RootLayout({
